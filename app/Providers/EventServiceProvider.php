b27e659e30cd404df06f7804270eb4f795adaa0e
Mohammad Ibrahim <112249309+awesomecoderdev@users.noreply.github.com>
2022-11-27 23:11:51 +0600

    Build shopping module

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Order;
 use App\Models\User;
+use App\Observers\OrderObserver;
 use App\Observers\UserObserver;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
@@ -29,8 +31,10 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // process userObserver
+        // UserObserver
         User::observe(UserObserver::class);
+        // OrderObserver
+        Order::observe(OrderObserver::class);
     }
 
     /**
