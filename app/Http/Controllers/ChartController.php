720d7e3d8ff02daf8ba0eb0a601d21ef1910d0f6
Mohammad Ibrahim <112249309+awesomecoderdev@users.noreply.github.com>
2022-12-23 14:16:17 +0600

    Updated Chart Cache

--- app/Http/Controllers/ChartController.php
+++ app/Http/Controllers/ChartController.php
@@ -6,6 +6,8 @@ use Carbon\Carbon;
 use App\Models\Order;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Http\Response as JsonResponse;
 
@@ -20,19 +22,25 @@ class ChartController extends Controller
      */
     public function orders(Request $request)
     {
+        abort_if(!Auth::user()->supperadmin(), \Illuminate\Http\Response::HTTP_UNAUTHORIZED, __("Unauthorized Access."));
 
-        $orders = Order::where("user_id", 1)->select(DB::raw('count(id) as `count`'), DB::raw("CONCAT_WS('-',DAY(created_at),MONTH(created_at),YEAR(created_at)) as date"))
-            ->groupBy('date')
-            ->orderBy("count")
-            ->get();
+        $cache_ttl = 5; // cache timer
+        $orders = Cache::remember('orders_chart', 60 * $cache_ttl, function () {
+            $orders_chart = Order::select(DB::raw('count(id) as `count`'), DB::raw("CONCAT_WS('-',DAY(created_at),MONTH(created_at),YEAR(created_at)) as date"))
+                ->groupBy('date')
+                ->orderBy("count")
+                ->get();
 
-        $orders = $orders->groupBy(function ($date) {
-            return Carbon::parse($date->date)->format('m-Y');
-            // return Carbon::parse($date->date)->format('F Y');
-        }, false)->toArray();
+            $orders_chart = $orders_chart->groupBy(function ($date) {
+                return Carbon::parse($date->date)->format('m-Y');
+            }, false)->toArray();
 
-        // $orders = array_chunk($orders, 1, true);
-        $startPoint = Order::select('created_at')->orderBy('created_at', 'asc')->first();
+            return $orders_chart;
+        });
+
+        $startPoint = cache::remember("chart_start", 60 * $cache_ttl, function () {
+            return Order::select('created_at')->orderBy('created_at', 'asc')->first();
+        });
 
         return Response::json([
             "success" => true,
