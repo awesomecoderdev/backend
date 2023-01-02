1c4372cd53b91d69612bf0ab404fde023841a997
Mohammad Ibrahim <112249309+awesomecoderdev@users.noreply.github.com>
2023-01-03 03:05:11 +0600

    Updated Notifications

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -27,7 +27,7 @@
                                     <div class="col-span-6 sm:col-span-3">
                                         <label for="first_name"
                                             class="block text-sm font-medium text-gray-700 dark:text-white">{{ __('First name') }}</label>
-                                        <input type="text" readonly name="first_name" id="first_name"
+                                        <input type="text" name="first_name" id="first_name"
                                             autocomplete="given-name" value="{{ old('first_name') }}"
                                             class="mt-1 block w-full rounded-md @error('first_name') border-red-200 ring-1 ring-red-500 focus:border-red-500 focus:ring-red-500 @else focus:border-primary-500 focus:ring-primary-500 border-gray-200 dark:border-slate-800 @enderror dark:bg-gray-800 shadow-sm  sm:text-sm">
                                         {{-- @error('first_name')
@@ -40,8 +40,8 @@
                                     <div class="col-span-6 sm:col-span-3">
                                         <label for="last_name"
                                             class="block text-sm font-medium text-gray-700 dark:text-white">{{ __('Last name') }}</label>
-                                        <input type="text" readonly name="last_name" id="last_name"
-                                            autocomplete="family-name" value="{{ old('last_name') }}"
+                                        <input type="text" name="last_name" id="last_name" autocomplete="family-name"
+                                            value="{{ old('last_name') }}"
                                             class="mt-1 block w-full rounded-md @error('last_name') border-red-200 ring-1 ring-red-500 focus:border-red-500 focus:ring-red-500 @else focus:border-primary-500 focus:ring-primary-500 border-gray-200 dark:border-slate-800 @enderror  dark:bg-gray-800  shadow-sm  sm:text-sm">
 
                                         {{-- @error('last_name')
@@ -54,8 +54,8 @@
                                     <div class="col-span-6 sm:col-span-3">
                                         <label for="email-address"
                                             class="block text-sm font-medium text-gray-700 dark:text-white">{{ __('Email address') }}</label>
-                                        <input type="text" readonly name="email-address" id="email-address"
-                                            autocomplete="email" value="{{ old('email') }}" {{-- class="mt-1 block w-full rounded-md border-gray-200 dark:border-slate-800 shadow-sm focus:border-primary-500 focus:ring-primary-500 sm:text-sm  dark:bg-gray-800 " --}}
+                                        <input type="text" name="email" id="email-address" autocomplete="email"
+                                            value="{{ old('email') }}" {{-- class="mt-1 block w-full rounded-md border-gray-200 dark:border-slate-800 shadow-sm focus:border-primary-500 focus:ring-primary-500 sm:text-sm  dark:bg-gray-800 " --}}
                                             class="mt-1 block w-full rounded-md @error('email') border-red-200 ring-1 ring-red-500 focus:border-red-500 focus:ring-red-500 @else focus:border-primary-500 focus:ring-primary-500 border-gray-200 dark:border-slate-800 @enderror  dark:bg-gray-800  shadow-sm  sm:text-sm">
                                         {{-- @error('email')
                                             <p class="mt-2 text-sm text-red-600 dark:text-red-500">
@@ -71,8 +71,8 @@
                                             'class' =>
                                                 ' mt-1 block w-full rounded-md border border-gray-200 dark:border-slate-800 bg-white py-2 px-3 shadow-sm focus:border-primary-500 focus:outline-none focus:ring-primary-500 sm:text-sm  dark:bg-gray-800 ',
                                             'id' => 'country',
-                                            'autocomplete' => 'country-name',
-                                            'disabled' => true,
+                                            'autocomplete' => 'country',
+                                            // 'disabled' => true,
                                         ]) !!}
                                         @error('country')
                                             <p class="mt-2 text-sm text-red-600 dark:text-red-500">
@@ -81,10 +81,37 @@
                                         @enderror
                                     </div>
 
+                                    <div class="col-span-6 sm:col-span-3">
+                                        <label for="password"
+                                            class="block text-sm font-medium text-gray-700 dark:text-white">{{ __('Password') }}</label>
+                                        <input type="password" name="password" id="password" autocomplete="password"
+                                            value="{{ old('password') }}"
+                                            class="mt-1 block w-full rounded-md @error('password') border-red-200 ring-1 ring-red-500 focus:border-red-500 focus:ring-red-500 @else focus:border-primary-500 focus:ring-primary-500 border-gray-200 dark:border-slate-800 @enderror dark:bg-gray-800 shadow-sm  sm:text-sm">
+                                        {{-- @error('password')
+                                            <p class="mt-2 text-sm text-red-600 dark:text-red-500">
+                                                <span class="font-medium">{{ __('Oops!') }} </span>{{ __($message) }}
+                                            </p>
+                                        @enderror --}}
+                                    </div>
+
+                                    <div class="col-span-6 sm:col-span-3">
+                                        <label for="confirmed"
+                                            class="block text-sm font-medium text-gray-700 dark:text-white">{{ __('Confirm password') }}</label>
+                                        <input type="password" name="confirmed" id="confirmed" autocomplete="confirmed"
+                                            value="{{ old('confirmed') }}"
+                                            class="mt-1 block w-full rounded-md @error('confirmed') border-red-200 ring-1 ring-red-500 focus:border-red-500 focus:ring-red-500 @else focus:border-primary-500 focus:ring-primary-500 border-gray-200 dark:border-slate-800 @enderror  dark:bg-gray-800  shadow-sm  sm:text-sm">
+
+                                        {{-- @error('confirmed')
+                                            <p class="mt-2 text-sm text-red-600 dark:text-red-500">
+                                                <span class="font-medium">{{ __('Oops!') }} </span>{{ __($message) }}
+                                            </p>
+                                        @enderror --}}
+                                    </div>
+
                                     <div class="col-span-6">
                                         <label for="street-address"
                                             class="block text-sm font-medium text-gray-700 dark:text-white">{{ __('Street address') }}</label>
-                                        <input type="text" readonly name="street-address" id="street-address"
+                                        <input type="text" name="street-address" id="street-address"
                                             autocomplete="street-address"
                                             class="mt-1 block w-full rounded-md @error('address') border-red-200 ring-1 ring-red-500 focus:border-red-500 focus:ring-red-500 @else focus:border-primary-500 focus:ring-primary-500 border-gray-200 dark:border-slate-800 @enderror  dark:bg-gray-800 shadow-sm  sm:text-sm">
                                         @error('address')
@@ -97,7 +124,7 @@
                                     <div class="col-span-6 sm:col-span-6 lg:col-span-2">
                                         <label for="city"
                                             class="block text-sm font-medium text-gray-700 dark:text-white">{{ __('City') }}</label>
-                                        <input type="text" readonly name="city" id="city"
+                                        <input type="text" name="city" id="city"
                                             autocomplete="address-level2"
                                             class="mt-1 block w-full rounded-md @error('city') border-red-200 ring-1 ring-red-500 focus:border-red-500 focus:ring-red-500 @else focus:border-primary-500 focus:ring-primary-500 border-gray-200 dark:border-slate-800 @enderror  dark:bg-gray-800  shadow-sm  sm:text-sm">
 
@@ -111,7 +138,7 @@
                                     <div class="col-span-6 sm:col-span-3 lg:col-span-2">
                                         <label for="region"
                                             class="block text-sm font-medium text-gray-700 dark:text-white">{{ __('State / Province') }}</label>
-                                        <input type="text" readonly name="region" id="region"
+                                        <input type="text" name="region" id="region"
                                             autocomplete="address-level1"
                                             class="mt-1 block w-full rounded-md @error('state') border-red-200 ring-1 ring-red-500 focus:border-red-500 focus:ring-red-500 @else focus:border-primary-500 focus:ring-primary-500 border-gray-200 dark:border-slate-800 @enderror dark:bg-gray-800  shadow-sm  sm:text-sm">
                                         @error('state')
@@ -124,7 +151,7 @@
                                     <div class="col-span-6 sm:col-span-3 lg:col-span-2">
                                         <label for="postal-code"
                                             class="block text-sm font-medium text-gray-700 dark:text-white">{{ __('ZIP / Postal code') }}</label>
-                                        <input type="text" readonly name="postal-code" id="postal-code"
+                                        <input type="text" name="postal-code" id="postal-code"
                                             autocomplete="postal-code"
                                             class="mt-1 block w-full rounded-md @error('zip') border-red-200 ring-1 ring-red-500 focus:border-red-500 focus:ring-red-500 @else focus:border-primary-500 focus:ring-primary-500 border-gray-200 dark:border-slate-800 @enderror  dark:bg-gray-800 shadow-sm  sm:text-sm">
                                         @error('zip')
