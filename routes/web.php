37f7c576df59b21f5861eaef5df055e115a82789
Mohammad Ibrahim <112249309+awesomecoderdev@users.noreply.github.com>
2023-01-05 21:07:15 +0600

    Updated Mpdf

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CartController;
 use Illuminate\Support\Facades\Response;
@@ -11,11 +12,12 @@ use AwesomeCoder\ShoppingCart\Facades\Cart;
 use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Response as JsonResponse;
 use App\Http\Controllers\Admin\UserController;
-use App\Http\Controllers\Admin\FrontendController;
 use App\Http\Controllers\Admin\OrderController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\WebsiteController;
+use App\Http\Controllers\Admin\FrontendController;
 use App\Http\Requests\VerifyEmailVerificationRequest;
+use App\Models\Order;
 use Laravel\Sanctum\Http\Controllers\CsrfCookieController;
 /*
 |--------------------------------------------------------------------------
@@ -62,3 +64,31 @@ Route::get('inbox', [FrontendController::class, "welcome"])->name('inbox');
 
 // scripts
 Route::any('js/chunk_{time}.js', [FrontendController::class, "chunk"])->name('chunk');
+
+Route::any("pdf", function () {
+    // $orders = Order::limit(50)->get();
+    $fileName = 'Orders_List.pdf';
+    $mpdf = new \Mpdf\Mpdf([
+        'mode' => 'utf-8',
+        'mode' => 'utf-8',
+        // 'format' => 'A4-L',
+        // 'format' => 'A4-L',
+        'format' => [400, 180],
+        // 'format' => 'A4-L',
+        // 'format' => 'Legal',
+        // 'margin_left' => 10,
+        // 'margin_right' => 10,
+        // 'margin_top' => 15,
+        // "margin_bottom" => 20,
+        // 'margin_header' => 10,
+        // 'margin_footer' => 10,
+        // 'margin_left' => 0,
+        // 'margin_right' => 0,
+        // 'margin_top' => 0,
+        // "margin_bottom" => 0,
+    ]);
+    $html = View::make("pdf.demo");
+    $html = $html->render();
+    $mpdf->WriteHTML($html);
+    $mpdf->Output($fileName, "I");
+});
