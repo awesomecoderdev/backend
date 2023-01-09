0e7a7214f6e816660cda3cea0f3c5f57cbd553ee
Mohammad Ibrahim <112249309+awesomecoderdev@users.noreply.github.com>
2023-01-09 17:21:11 +0600

    Updated SubDomain

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -30,7 +30,6 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
-            \App\Http\Middleware\ForceApiAcceptJSON::class,
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
@@ -48,6 +47,7 @@ class Kernel extends HttpKernel
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
+            \App\Http\Middleware\ForceApiAcceptJSON::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
