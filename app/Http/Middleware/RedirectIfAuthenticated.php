2c6c6107b6ad953ea974a90fa33b4af0ce721572
Mohammad Ibrahim <112249309+awesomecoderdev@users.noreply.github.com>
2023-01-11 19:02:18 +0600

    Updated Auth Protection

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -8,7 +8,6 @@ use App\Http\Resources\UserResource;
 use Illuminate\Support\Facades\Auth;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Support\Facades\Response;
-use Illuminate\Http\Response as JsonResponse;
 
 class RedirectIfAuthenticated
 {
@@ -23,20 +22,23 @@ class RedirectIfAuthenticated
     public function handle(Request $request, Closure $next, ...$guards)
     {
         $guards = empty($guards) ? [null] : $guards;
+        $subdomain = strtok(preg_replace('#^https?://#', '', rtrim($request->url(), '/')), '.');
+        $error = $subdomain == "api";
 
-        // for production only
-        // foreach ($guards as $guard) {
-        //     if (Auth::guard($guard)->check()) {
-        //         // return redirect(RouteServiceProvider::HOME);
-
-        //         return Response::json([
-        //             "success" => true,
-        //             "status"    => JsonResponse::HTTP_NOT_ACCEPTABLE,
-        //             "message"   => "You are Already Logged in",
-        //             "redirect"  => true
-        //         ], JsonResponse::HTTP_OK);
-        //     }
-        // }
+        foreach ($guards as $guard) {
+            if (Auth::guard($guard)->check()) {
+                if ($error) {
+                    return Response::json([
+                        "success" => true,
+                        "status"    => \Illuminate\Http\Response::HTTP_NOT_ACCEPTABLE,
+                        "message"   => __("You are already logged in"),
+                        "redirect"  => true
+                    ],  \Illuminate\Http\Response::HTTP_OK);
+                } else {
+                    return redirect(RouteServiceProvider::HOME);
+                }
+            }
+        }
 
         return $next($request);
     }
