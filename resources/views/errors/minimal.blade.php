9ee33707a23f4677f9c1732bb59627e5a24e7e1e
Mohammad Ibrahim <112249309+awesomecoderdev@users.noreply.github.com>
2023-01-11 16:23:14 +0600

    Updated Error Page

--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -1,19 +1,47 @@
-<x-client-layout>
-    @section('head')
-        <title>@yield('title')</title>
-    @endsection
-    <div
-        class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-800 sm:items-center sm:pt-0">
-        <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
-            <div class="flex items-center pt-8 sm:justify-start sm:pt-0">
-                <div class="px-4 text-lg text-gray-500 border-r border-gray-400 tracking-wider">
-                    @yield('code')
-                </div>
+@php
+    $path = strtok(preg_replace('#^https?://#', '', rtrim(Request::url(), '/')), '.');
+@endphp
 
-                <div class="ml-4 text-lg text-gray-500 uppercase tracking-wider">
-                    @yield('message')
+@if (Auth::check() && Auth::user()->admin() && $path == 'admin')
+    <x-app-layout>
+        @section('head')
+            <title>@yield('title')</title>
+        @endsection
+        <x-content>
+            <div
+                class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-800 sm:items-center sm:pt-0">
+                <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
+                    <div class="flex items-center pt-8 sm:justify-start sm:pt-0">
+                        <div class="px-4 text-lg text-gray-500 border-r border-gray-400 tracking-wider">
+                            @yield('code')
+                        </div>
+
+                        <div class="ml-4 text-lg text-gray-500 uppercase tracking-wider">
+                            @yield('message')
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </x-content>
+    </x-app-layout>
+@else
+    <x-client-layout>
+        @section('head')
+            <title>@yield('title')</title>
+        @endsection
+        <div
+            class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-800 sm:items-center sm:pt-0">
+            <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
+                <div class="flex items-center pt-8 sm:justify-start sm:pt-0">
+                    <div class="px-4 text-lg text-gray-500 border-r border-gray-400 tracking-wider">
+                        @yield('code')
+                    </div>
+
+                    <div class="ml-4 text-lg text-gray-500 uppercase tracking-wider">
+                        @yield('message')
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</x-client-layout>
+    </x-client-layout>
+@endif
