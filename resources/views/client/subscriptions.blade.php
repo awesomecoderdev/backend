f8c0a8c029c4cfb62ea3f4404d508368897aeed4
Mohammad Ibrahim <112249309+awesomecoderdev@users.noreply.github.com>
2023-01-24 03:42:01 +0600

    Updated Subscriptions UI

--- resources/views/client/subscriptions.blade.php
+++ resources/views/client/subscriptions.blade.php
@@ -10,8 +10,8 @@
         <div class="relative">
             <form action="{{ route('client.subscriptions.payment') }}" id="checkout" method="post">
                 @csrf
-                <ul class="grid w-full gap-6 md:grid-cols-3">
-                    <li x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
+                <div class="grid w-full gap-6 md:grid-cols-3">
+                    <div x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
                         <input type="radio" id="free" name="plan" @checked(true) value="free"
                             class="hidden peer">
                         <label for="free"
@@ -127,8 +127,8 @@
                                 </div>
                             </div>
                         </label>
-                    </li>
-                    <li x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
+                    </div>
+                    <div x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
 
                         <input type="radio" id="essential" name="plan" value="price_1MTSfvIX4CRni5u3wdGvV2c2"
                             class="hidden peer">
@@ -250,8 +250,8 @@
                                 </div>
                             </div>
                         </label>
-                    </li>
-                    <li x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
+                    </div>
+                    <div x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
                         <input type="radio" id="premium" name="plan" value="price_1MTSfvIX4CRni5u37Kpj3wLK"
                             class="hidden peer">
                         <label for="premium"
@@ -374,38 +374,53 @@
                                 </div>
                             </div>
                         </label>
-                    </li>
-                </ul>
-
-                <div class="relative my-2 p-0.5 text-gray-900 dark:text-white">
-                    <label for="card" class="contents text-base font-medium py-2">
-                        {{ __('Credit or debit card') }}
-                    </label>
-                    <div id="card"
-                        class=" bg-white dark:bg-slate-800 border text-red p-3 mt-2 block w-full rounded-md focus:border-primary-500 focus:ring-primary-500 border-gray-200 dark:border-slate-500 shadow-sm sm:text-sm">
-                        <!-- A Stripe Element will be inserted here. -->
                     </div>
-
-                    <!-- Used to display Element errors. -->
-                    <div role="alert">
-                        <p class="mt-2 text-sm text-red-600 dark:text-red-500">
-                            <span id="card-errors" class="font-medium">
-                            </span>
-                        </p>
-                    </div>
-
                 </div>
 
-                <div class="relative">
-                    <button type="submit"
-                        class="w-full px-4 py-2 mt-6 tracking-wide text-white capitalize transition-colors duration-300 transform bg-primary-600 rounded-md hover:bg-primary-500 focus:outline-none focus:bg-primary-500 focus:ring focus:ring-primary-300 focus:ring-opacity-80"
-                        data-secret="{{ $intent->client_secret }}">{{ __('Subscribe Now') }}</button>
+                <div class="grid w-full gap-6 md:grid-cols-3">
+                    <div class="col-span-2 relative p-5">
+                    </div>
+                    <div class="relative my-2 p-1 text-gray-900 dark:text-white">
+                        <div class="relative">
+                            <label for="card-holder-name"
+                                class="block text-sm font-medium text-gray-700 dark:text-white">{{ __('Cardholder\'s name') }}</label>
+                            <input type="text" name="card-holder-name" id="card-holder-name"
+                                autocomplete="address" placeholder="{{ __('Cardholder\'s name') }}"
+                                class="my-1 p-2 block w-full rounded-md @error('card-holder-name') border-red-200 ring-1 ring-red-500 focus:border-red-500 focus:ring-red-500 @else focus:border-primary-500 focus:ring-primary-500 border-gray-200 dark:border-slate-500 @enderror  dark:bg-gray-800 shadow-sm  sm:text-sm">
+                        </div>
+
+                        <label for="card" class="contents text-base font-medium py-2">
+                            {{ __('Credit or debit card') }}
+                        </label>
+                        <div id="card"
+                            class=" bg-white dark:bg-slate-800 border text-red p-2.5 mt-2 block w-full rounded-md focus:border-primary-500 focus:ring-primary-500 border-gray-200 dark:border-slate-500 shadow-sm sm:text-sm">
+                            <!-- A Stripe Element will be inserted here. -->
+                        </div>
+                        <!-- Used to display Element errors. -->
+                        <div role="alert">
+                            <p class="mt-2 text-sm text-red-600 dark:text-red-500">
+                                <span id="card-errors" class="font-medium">
+                                </span>
+                            </p>
+                        </div>
+
+
+                        <div class="relative">
+                            <button type="submit" id="checkoutBtn"
+                                class="w-full px-4 py-2 mt-6 tracking-wide text-white capitalize transition-colors duration-300 transform bg-primary-600 rounded-md hover:bg-primary-500 focus:outline-none focus:bg-primary-500 focus:ring focus:ring-primary-300 focus:ring-opacity-80"
+                                data-secret="{{ $intent->client_secret }}">{{ __('Subscribe Now') }}</button>
+                        </div>
+
+                    </div>
+
                 </div>
             </form>
         </div>
         <script>
             let stripe{{ $key }} = Stripe("{{ config('cashier.key') }}");
             let elements{{ $key }} = stripe{{ $key }}.elements();
+            var cardHolderName = document.getElementById('card-holder-name');
+            var clientSecret = document.getElementById('checkoutBtn').dataset.secret;
             console.log('darkMode', isDarkMode);
 
             // Create an instance of the card Element.
@@ -440,23 +455,30 @@
                 .addEventListener('submit', async (event) => {
                     event.preventDefault();
 
-                    let {
-                        token,
+                    const {
+                        setupIntent,
                         error
-                    } = await stripe{{ $key }}.createToken(card{{ $key }});
+                    } = await stripe{{ $key }}.confirmCardSetup(
+                        clientSecret, {
+                            payment_method: {
+                                card: card{{ $key }},
+                                billing_details: {
+                                    name: cardHolderName.value
+                                }
+                            }
+                        }
+                    );
 
                     if (error) {
                         // Inform the customer that there was an error.
                         let errorElement{{ $key }} = document.getElementById('card-errors');
                         let notifications{{ $key }} = document.getElementById('notifications');
                         errorElement{{ $key }}.innerText = `{{ __('Oops!') }} ${error.message}`;
-                        notifications{{ $key }}.innerHTML =
-                            `<x-alert delay="4" end="4" autoclose='true' type="error" title="Error!" message="${error.message}" />`;
                     } else {
-                        // Send the token to your server.
-                        stripe{{ $key }}TokenHandler(token);
+                        // The card has been verified successfully...
                     }
                 });
+
 
             let stripe{{ $key }}TokenHandler = (token) => {
                 // Insert the token ID into the form so it gets submitted to the server
@@ -474,3 +496,51 @@
     </x-client>
 
 </x-client-layout>
+
+@php
+    return false;
+@endphp
+<script>
+    // let {
+    //     token,
+    //     error
+    // } = await stripe{{ $key }}.createToken(card{{ $key }});
+
+    // if (error) {
+    //     // Inform the customer that there was an error.
+    //     let errorElement{{ $key }} = document.getElementById('card-errors');
+    //     let notifications{{ $key }} = document.getElementById('notifications');
+    //     errorElement{{ $key }}.innerText = `{{ __('Oops!') }} ${error.message}`;
+    //     notifications{{ $key }}.innerHTML =
+    //         `<x-alert delay="4" end="4" autoclose='true' type="error" title="Error!" message="${error.message}" />`;
+    // } else {
+    //     // Send the token to your server.
+    //     stripe{{ $key }}TokenHandler(token);
+
+    // }
+
+    // const {
+    //     setupIntent,
+    //     error
+    // } = await stripe.confirmCardSetup(
+    //     clientSecret, {
+    //         payment_method: {
+    //             card: cardElement,
+    //             billing_details: {
+    //                 name: cardHolderName.value
+    //             }
+    //         }
+    //     }
+    // );
+
+    // if (error) {
+    //     // Inform the customer that there was an error.
+    //     let errorElement{{ $key }} = document.getElementById('card-errors');
+    //     let notifications{{ $key }} = document.getElementById('notifications');
+    //     errorElement{{ $key }}.innerText = `{{ __('Oops!') }} ${error.message}`;
+    //     notifications{{ $key }}.innerHTML =
+    //         `<x-alert delay="4" end="4" autoclose='true' type="error" title="Error!" message="${error.message}" />`;
+    // } else {
+    //     // The card has been verified successfully...
+    // }
+</script>
